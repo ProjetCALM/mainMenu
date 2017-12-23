9cb01582c7ae0bfa5edf7c406e086b07e95a0e34
Jrbdxa <34715106+Jrbdxa@users.noreply.github.com>
2017-12-23 02:21:22 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: prico
+ *
+ * Created on 22 décembre 2017, 23:18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "headermenu.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int id=0; //permet de suivre le nombre de joueurs
+    joueur rep[REP];
+    initRep;
+    id=choiceMainMenu(rep, id);
+    return (EXIT_SUCCESS);
+}
+
